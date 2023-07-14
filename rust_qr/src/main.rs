57b9475bccdd6ad7236ff632314ad4b1e3985415
KwiiHours <41028518+KwiiHours@users.noreply.github.com>
2023-07-14 22:13:32 +1200

    Updated ReadMe, added Tera templates

--- rust_qr/src/main.rs
+++ rust_qr/src/main.rs
@@ -1,48 +1,34 @@
-
-// basic rocket app that handles a get index, get /<short-url> and post /
-
 #[macro_use] extern crate rocket;
 
 use rocket::fs::FileServer;
-use rocket::fs::NamedFile;
-use std::path::{Path, PathBuf};
-use rocket::form::Form;
-
-#[derive(FromForm)]
-struct Url {
-    big_url: String,
-}
+use rocket_dyn_templates::Template;
+use std::collections::HashMap;
 
 #[get("/")]
-async fn index() -> Option<NamedFile> { // Display the index.html file
-    NamedFile::open(Path::new("static/index.html")).await.ok()
+fn index() -> Template {
+    Template::render("index", &HashMap::<String, String>::new())
 }
 
 #[get("/<short_url>")]
-fn get_big_url(short_url: String) -> &'static str {
-    // Get the short url from database
-    // Redirect to the long url
-    // If the short url is not found, return a 404
-    "short url"
+fn get_big_url(short_url: String) -> Template {
+    let mut context = HashMap::new();
+    context.insert("short_url", short_url);
+
+    Template::render("short_url", &context)
 }
 
 #[post("/?<big_url>")]
-fn post_url(big_url: &str) -> String {
-    // Generate a short url
-    // Save the short url and long url to database
-    // Return the short url
-    "big url"
+fn post_url(big_url: String) -> Template {
+    let mut context = HashMap::new();
+    context.insert("big_url", big_url);
+
+    Template::render("big_url", &context)
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, get_big_url, post_url])
-        .mount("/static", rocket::fs::FileServer::from("static"))
-        // .mount("/static", FileServer::from("static"))
-        // .attach(rocket::fairing::AdHoc::on_attach("Database Config", |rocket| {
-        //     let db_url = rocket.config().get_string("db_url").unwrap();
-        //     let pool = db::init_pool(db_url).expect("db pool");
-        //     Ok(rocket.manage(pool))
-        // }))
+        .mount("/static", FileServer::from("static"))
+        .attach(Template::fairing())
 }
